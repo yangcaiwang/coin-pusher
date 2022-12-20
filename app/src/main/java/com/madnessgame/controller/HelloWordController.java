da9b5c8218f47c2cecb29e622478a6eed8a3ba44
yangcaiwang <ycw123129@163.com>
2022-12-20 20:41:09 +0800

    更新docker镜像

--- app/src/main/java/com/madnessgame/controller/HelloWordController.java
+++ app/src/main/java/com/madnessgame/controller/HelloWordController.java
@@ -1,7 +1,7 @@
 package com.madnessgame.controller;
 
 import com.madnessgame.response.R;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
  * <p>
  */
 @RestController
-@RequestMapping("/coin-pusher/helloWord")
 public class HelloWordController {
+    @GetMapping("/coin-pusher/helloWord")
     public R<String> helloWord() {
         return R.ok("hello,word!");
     }
